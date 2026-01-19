9fe146440252a9c67df13e7bfa996f95d4f4bc66
learis <unsalerkal@gmail.com>
2026-01-19 09:51:55 +0300

    Add catch-all route redirect to login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -34,6 +34,7 @@ function App() {
           <Route path="settings" element={<Settings />} />
           <Route path="" element={<Navigate to="/dashboard" replace />} />
         </Route>
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   );
